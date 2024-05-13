a047db5d84ba0b8c47bace2297c64fb09b9bc87a
Lev <lev@brandclick.com>
2024-05-13 22:50:31 +0300

    feat: text add list, product add list

--- src/components/ProductAdsList/ProductAdItem.tsx
+++ src/components/ProductAdsList/ProductAdItem.tsx
@@ -24,31 +24,41 @@ export const ProductAdItem: React.FC<ProductAdItemProps> = ({
   rating,
 }) => {
   title = title.length > 23 ? title.slice(0, 23) + "..." : title;
-  sellername = sellername.length > 13 ? sellername.slice(0, 13) + "..." : sellername;
-  additional = additional && additional.length > 14 ? additional?.slice(0, 14) + "..." : additional;
-  const rateStars = !rating || rating < 0.5 ? <></> : <StarRating rating={rating} />;
+  sellername =
+    sellername.length > 13 ? sellername.slice(0, 13) + "..." : sellername;
+  additional =
+    additional && additional.length > 14
+      ? additional?.slice(0, 14) + "..."
+      : additional;
+  const rateStars =
+    !rating || rating < 0.5 ? <></> : <StarRating rating={rating} />;
 
   return (
     <a href={url}>
       <div className="w-28 h-56 border border-gray-300 rounded-md shadow hover:shadow-lg hover:scale-105 cursor-pointer">
-        <Image
-          className="object-cover"
-          src={photoUrl}
-          alt="DealFinder logo"
-          width={128}
-          height={128}
-          priority
-        />
+        <div className="min-h-[100px] max-w-[112px] flex items-center justify-center">
+          <Image
+            className="object-cover"
+            src={photoUrl}
+            alt="DealFinder logo"
+            width={112}
+            height={100}
+            priority
+          />
+        </div>
         <div className="p-1">
           <h2 className="text-sm leading-4 mb-1 text-dark-blue hover:text-dark-orange hover:underline">
             {title}
           </h2>
           <p className="text-sm mb-1 text-green-900">{sellername}</p>
           <p className="text-sm mb-1">${price}</p>
-          {additional && <p className="text-xs leading-5 text-gray-800">{additional}</p>}
+          {additional && (
+            <p className="text-xs leading-5 text-gray-800">{additional}</p>
+          )}
           {rateStars}
         </div>
       </div>
     </a>
   );
 };
+
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -40,75 +40,96 @@ const DUMMY_PRODUCT_ADs = [
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    rating: 10
+    rating: 10,
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    rating: 5
+    rating: 5,
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    additional: "50% price drop"
+    additional: "50% price drop",
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    rating: 7
+    rating: 7,
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    rating: 0
+    rating: 0,
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    additional: "50% price drop"
+    additional: "50% price drop",
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
-    additional: "50% price drop"
+    additional: "50% price drop",
   },
   {
     title: "Barbecue portable grill",
     url: "https://www.car-mart.com/",
-    photoUrl: "/grill.png",
+    photoUrl: "/car.jpg",
     sellername: "grillshop.com",
     price: 499,
   },
 ];
 
+const DUMMY_RELATED_SEARCHES = [
+  "buy car buy car buy car buy car buy car buy car buy car buy cae",
+  "usa cars for sale",
+  "car simulator 2",
+  "download car for sale",
+  "formula 1",
+  "exotic lamborghini car",
+  "rent a car",
+];
+
 export const SearchResults = () => {
   return (
-    <div className="w-full flex flex-row">
-      <div className="min-w-[608px]">
-        <TextAdList ads={DUMMY_TEXT_ADs} />
+    <div>
+      <div className="pl-5">
+        <span className="text-sm text-gray-600 inline-block overflow-hidden whitespace-nowrap">
+          About 304 000 000 results
+        </span>
       </div>
-      <ProductAdList title="Shops for Portable Barbecue Grill" ads={DUMMY_PRODUCT_ADs} />
+      <div className="w-full flex flex-row pt-2">
+        <TextAdList ads={DUMMY_TEXT_ADs} />
+        <div className="min-w-[500px] max-w-[500px] ml-28">
+          <ProductAdList
+            title="Shops for Portable Barbecue Grill"
+            ads={DUMMY_PRODUCT_ADs}
+          />
+        </div>
+      </div>
     </div>
   );
 };
+
