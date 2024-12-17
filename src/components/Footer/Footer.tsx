b22d80ec74376f3d94b0f1d2207d0e4e142b1e64
nextdevio <105893825+nextdevio@users.noreply.github.com>
2024-12-17 13:09:23 -0800

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,13 +2,13 @@ export const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full flex flex-row justify-center py-3 bg-gray-100">
       <div className="min-w-[300px] flex flex-row text-gray-700">
-        <a className="hover:underline" href="#">
+        <a className="hover:underline" href="https://brandclick.com/privacy">
           Privacy
         </a>
         <a className="hover:underline ml-4 mr-4" href="https://brandclick.com/terms-of-servce">
           Terms of Service
         </a>
-        <a className="hover:underline" href="https://brandclick.com/privacy">
+        <a className="hover:underline" href="https://brandclick.com/contact">
           Contact Us
         </a>
       </div>
