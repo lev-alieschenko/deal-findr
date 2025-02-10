2d245fb03d97c25ad85fd255446faeb1795f830d
Zarina1007 <farizayandiyeva@gmail.com>
2025-02-10 13:25:01 +0000

    loading bar update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,17 @@ import { useEffect, useState } from 'react';
 export const runtime = 'edge';
 
 const SearchResultsLoading = () => (
-  <div className='p-4'>
-    <div className='animate-pulse space-y-4'>
-      <div className='h-4 bg-gray-200 rounded w-3/4'></div>
-      <div className='h-4 bg-gray-200 rounded w-1/2'></div>
-      <div className='h-4 bg-gray-200 rounded w-5/6'></div>
-    </div>
+  <div className="p-6 space-y-4">
+    {[...Array(3)].map((_, index) => (
+      <div key={index} className="animate-pulse flex space-x-4">
+        <div className="w-16 h-16 bg-gray-300 rounded-lg"></div>
+
+        <div className="flex-1 space-y-3">
+          <div className="h-5 bg-gray-200 rounded w-3/4"></div>
+          <div className="h-4 bg-gray-200 rounded w-1/2"></div>
+        </div>
+      </div>
+    ))}
   </div>
 );
 
