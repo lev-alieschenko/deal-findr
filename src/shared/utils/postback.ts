912a724e4518580e6c00b220369999dd67e06c95
Zarina1007 <farizayandiyeva@gmail.com>
2025-02-13 02:24:59 +0000

    cors issue update for postback part

--- src/shared/utils/postback.ts
+++ src/shared/utils/postback.ts
@@ -2,8 +2,10 @@ export const sendPostback = async (cid: string) => {
   if (!cid) return;
 
   try {
-    await fetch(`https://addents-leasure.icu/postback?cid=${cid}`);
-    console.log('Postback sent');
+    await fetch(`https://addents-leasure.icu/postback?cid=${cid}`, {
+      mode: "no-cors"
+    });
+    console.log('Postback successfully sent!!');
   } catch (error) {
     console.error('Postback failed:', error);
   }
