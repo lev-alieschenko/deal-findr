30e36e7d4d6b4e7788638587424d508177fc7e80
Zarina1007 <farizayandiyeva@gmail.com>
2025-03-28 05:11:13 +0000

    add empty filter

--- src/components/common/getPParams.ts
+++ src/components/common/getPParams.ts
@@ -1,8 +1,8 @@
 export const getPParams = (searchParams: Record<string, string>) => {
   return Object.entries(searchParams)
-    .filter(([key]) => {
+    .filter(([key, value]) => {
       const lowerKey = key.toLowerCase();
-      return lowerKey !== "cid" && lowerKey !== "clickid" && lowerKey !== "subid";
+      return lowerKey !== "cid" && lowerKey !== "clickid" && lowerKey !== "subid" && value.trim() !== "";
     })
     .map(([_, value]) => value);
 };
