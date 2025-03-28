1f38a9199e8ae8d1bd64187e978bcb22b1f4113f
Zarina1007 <farizayandiyeva@gmail.com>
2025-03-28 05:42:57 +0000

    check referer value

--- src/app/api/refer/route.ts
+++ src/app/api/refer/route.ts
@@ -3,36 +3,35 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = 'edge';
 
 //specialReferrers
-const specialReferrers = ["yarb"];
+const specialReferrers = ["yarb.net"];
 const destination = "https://redirect-destination.com";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const referrer = searchParams.get("referrer") || request.headers.get("referer") || "";
-  console.log(referrer, "========referer======")
-  if (referrer && specialReferrers.some((domain) => referrer.includes(domain))) {
-    const redirectUrl = new URL(destination);
-    redirectUrl.searchParams.set("source", "redirect");
+  // if (referrer && specialReferrers.some((domain) => referrer.includes(domain))) {
+  //   const redirectUrl = new URL(destination);
+  //   redirectUrl.searchParams.set("source", "redirect");
 
-    return new NextResponse(
-      `
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <meta http-equiv="refresh" content="0; url=${redirectUrl.toString()}" />
-        <title>Redirecting...</title>
-        <script>window.location.href = "${redirectUrl.toString()}";</script>
-      </head>
-      <body>
-        <p>If you are not redirected automatically, click <a href="${redirectUrl.toString()}">here</a>.</p>
-      </body>
-      </html>
-    `,
-      {
-        headers: { "Content-Type": "text/html;charset=UTF-8" },
-      }
-    );
-  }
+  //   return new NextResponse(
+  //     `
+  //     <!DOCTYPE html>
+  //     <html>
+  //     <head>
+  //       <meta http-equiv="refresh" content="0; url=${redirectUrl.toString()}" />
+  //       <title>Redirecting...</title>
+  //       <script>window.location.href = "${redirectUrl.toString()}";</script>
+  //     </head>
+  //     <body>
+  //       <p>If you are not redirected automatically, click <a href="${redirectUrl.toString()}">here</a>.</p>
+  //     </body>
+  //     </html>
+  //   `,
+  //     {
+  //       headers: { "Content-Type": "text/html;charset=UTF-8" },
+  //     }
+  //   );
+  // }
 
   return new NextResponse(`This is the usual page content. Referrer: ${referrer}`, {
     headers: { "Content-Type": "text/plain" },
